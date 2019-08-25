d759acdfa019c8a898bb6fc96db77e55a62281f6
ZRainy <923446353@qq.com>
2019-08-25 15:51:14 +0800

    add zrainy_construct.h

---
+++ include/zrainy_alloc.h
@@ -0,0 +1,15 @@
+/*
+ * 空间配置器
+ *
+ * by Zrainy
+ */
+
+#ifndef ZRAINY_ALLOC_H
+#define ZRAINY_ALLOC_H
+
+
+namespace ZRainySTL{
+
+}//end namespace ZRainySTL
+#endif
+
---
+++ include/zrainy_construct.h
@@ -0,0 +1,41 @@
+/*
+ * 基本构造和析构函数
+ * 析构函数分两种，一种是接收一个简单指针的，另一种是接收一对迭代器，
+ * 对[first,last)范围内的对象都进行析构
+ * 
+ * by Zrainy
+ */
+
+#ifndef ZRAINY_CONSTRUCT_H_
+#define ZRAINY_CONSTRUCT_H_
+
+#include <new> //要使用placement new，必须包含该头文件
+
+
+namespace ZRainySTL{
+	//基本构造函数
+	template<class T1, class T2>
+	inline void construct(T1 *ptr, const T2& value){
+		new(ptr) T1(value);
+	}
+
+	//基本析构函数
+	template<class T>
+	inline void destroy(T *ptr){
+		ptr->~T();
+	}
+
+
+	//接收一对迭代器的析构函数
+	template<class ForwardIterator>
+	inline void destroy(ForwardIterator first, ForwardIterator last){
+		for(; first != last; ++first){
+			destroy(& *first);
+		}
+	}
+
+	//TODO 尚未针对traits型别进行完善，后续引入traits后完善。
+
+}// end namespace ZRainySTL
+
+#endif
