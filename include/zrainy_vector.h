24b148680adf4a0ad4415b50542205f3d05b5309
ZRainy <923446353@qq.com>
2019-10-29 23:06:58 +0800

    write vector #1

--- include/zrainy_vector.h
+++ include/zrainy_vector.h
@@ -2,6 +2,62 @@
 #define _ZRAINY_VECTOR_H_
 
 
+//#include "zrainy_type_traits.h"
+#include "zrainy_allocate.h"
+#include "zrainy_construct.h"
+#include "zrainy_iterator.h"
+
+
+namespace ZRainySTL{
+	
+	template<class T, class Alloc = allocator<T>>
+	//为了与stl中的容器区分，ZRainySTL中的所有容器添加前缀z
+	class zvector{
+	public:
+		//zvector的嵌套型别定义
+		typedef T					value_type;
+		typedef value_type*			pointer;
+		typedef value_type&			reference;
+		typedef const value_type&	const_reference;
+		typedef value_type*			iterator;
+		typedef const value_type*	const_iterator;
+		typedef size_t				size_type;
+		typedef ptrdiff_t			difference_type;
+		
+	private:
+		//空间配置器，这里我就直接使用allocator了
+		typedef Alloc data_allocator;
+		iterator start;//表示目前使用空间的头
+		iterator finish;//表示目前使用空间的尾
+		iterator end_of_storage;//表示目前可用空间的尾
+	
+	public:
+		//构造和析构函数,TODO
+		zvector():start(nullptr), finish(nullptr), end_of_storage(nullptr){}
+		zvector(size_type n, const T& value);
+		~zvector(){
+			destroy(start, finish);
+			deallocate();
+		}
+
+		//基本操作
+		iterator begin(){return start;}
+		iterator end(){return finish;}
+		size_type size(){return (size_type)(finish - start);}
+		bool empty() const {return begin() == end();}
+
+		void push_back(const T& v);
+		void pop_back();
+	
+	private:
+		void deallocate(){
+			if(start){
+				data_allocator::deallocate(start, end_of_storage - start);
+			}
+		}
+		
+	};// end class zvector
+}// end namespace ZRainySTL
 
 #endif //end ifnde
 
